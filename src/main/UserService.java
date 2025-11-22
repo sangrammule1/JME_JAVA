0705f378c349f534f008e3cc14f6c5d6b32e5f6c
sangrammule <sangrammule@localhost>
2025-11-22 16:43:32 +0530

    commit 5

--- src/main/UserService.java
+++ src/main/UserService.java
@@ -7,31 +7,8 @@ public class UserService {
             user.getProfile()
                 .getAddress()
                 .getStreet()
-package main;
-
-public class UserService {
-
-    public String format(User user) {
-        String s = null;
-        if (user != null && user.getProfile() != null && user.getProfile().getAddress() != null && user.getProfile().getAddress().getStreet() != null) {
-            s = user.getProfile()
-                .getAddress()
-                .getStreet()
                 .trim();
-        }
-        return s;
-    }
-    
-    public static void main(String[] args) {
-		UserService userService = new UserService();
-		Address address = new Address(null);
-		Profile profile = new Profile(address);
-		User user = new User(profile);
-		String format = userService.format(user);
-		System.out.println(format);
-	}
-}
-        return s;
+        return s.toUpperCase();
     }
     
     public static void main(String[] args) {
